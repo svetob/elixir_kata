bef7dbf0a27b154560d31f4b497514da4c406094
Tobias Ara Svensson <tobias.ara.svensson@meltwater.com>
2014-11-27 11:40:23 +0100

    Another way to do it

--- lib/elixir_kata.ex
+++ lib/elixir_kata.ex
@@ -1,4 +1,21 @@
 defmodule Romans do
+
+    def lits, do: [
+        {1000, "M"},
+        {900, "CM"},
+        {500, "D"},
+        {400, "CD"},
+        {100, "C"},
+        {90, "XC"},
+        {50, "L"},
+        {40, "XL"}, 
+        {10, "X"},
+        {9, "IX"},
+        {5, "V"},
+        {4, "IV"},
+        {1, "I"}
+    ]
+
     def fromArabic(num) do
         cond do
             num < 0 ->
@@ -7,32 +24,20 @@ defmodule Romans do
                 raise "max allowed is 3000"
             num == 0 ->
                 ""
-            num >= 1000 ->
-                "M" <> fromArabic(num-1000)
-            num >= 900 ->
-                "CM" <> fromArabic(num-900)
-            num >= 500 ->
-                "D" <> fromArabic(num-500)
-            num >= 400 ->
-                "CD" <> fromArabic(num-400)
-            num >= 100 ->
-                "C" <> fromArabic(num-100)
-            num >= 90 ->
-                "XC" <> fromArabic(num-90)
-            num >= 50 ->
-                "L" <> fromArabic(num-50)
-            num >= 40 ->
-                "XL" <> fromArabic(num-40)
-            num >= 10 ->
-                "X" <> fromArabic(num-10)
-            num >= 9 ->
-                "IX" <> fromArabic(num-9)
-            num >= 5 ->
-                "V" <> fromArabic(num-5)
-            num >= 4 ->
-                "IV" <> fromArabic(num-4)
-            num >= 1 ->
-                "I" <> fromArabic(num-1)
+            true ->
+                elem(pick(num), 0) <> fromArabic(elem(pick(num), 1))
+        end
+    end
+
+    def pick(num) do
+        pick(num, lits)
+    end
+
+    def pick(num, l) do
+        if num >= elem(hd(l), 0) do
+            {elem(hd(l), 1), num - elem(hd(l), 0)}
+        else
+            pick(num, tl(l))
         end
     end
 end
